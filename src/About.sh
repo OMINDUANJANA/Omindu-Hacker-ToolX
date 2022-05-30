bb8dccbc0f8976558928576e770a5eec85ae1079
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 11:04:07 +0530

    Update About.sh

--- src/About.sh
+++ src/About.sh
@@ -8,13 +8,13 @@ echo '
     │ >>   Hi, here you can see my telegram and github there       |
     |       will be questions you will write in telegram.          │
     │                                                              │
-    │ >>  Author   :  Misha Korzhik                                │
-    │     Github   :  https://github.com/mishakorzik               │
-    │     Telegram :  pseudonym @MishaKorzhikTelegram              │
-    |     Info     :  Trank for installing AllHackingTools         |
+    │ >>  Author   :  Omindu Anjana                                │
+    │     Github   :  https://github.com/OMINDUANJANA              │
+    |     Info     :  Tranks for installing Omindu-Hacker-ToolX    |
+    |              |
     │                                                              │
     └──────────────────────────────────────────────────────────────┘ 
 '|lolcat -a -d 10 -p 1.5 -s 40.0
-cd && cd AllHackingTools  
+cd && cd Omindu-Hacker-ToolX  
 python3 src/Timer1.py
 python2 src/aboutMenu.py
