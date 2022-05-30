2a0a60e416b50b90057a80569c18c34068fc94be
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 11:20:24 +0530

    🍒

--- src/CheckVersion.py
+++ src/CheckVersion.py
@@ -36,7 +36,7 @@ result = ''
 version = '2.7.4'
 
 def ver_check():
-	print(G + '[+]' + C + ' Checking the AllHackingTools for updates....', end='')
+	print(G + '[+]' + C + ' Checking the Omindu-Hacker-ToolX for updates....', end='')
 	ver_url = 'https://raw.githubusercontent.com/mishakorzik/AllHackingTools/main/Castom/version.txt'
 	try:
 		ver_rqst = requests.get(ver_url)
