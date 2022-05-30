ab416f1b687a20b5d221f7ebb4cacd9616ecc84e
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 10:57:45 +0530

    🖥️New Poson Update 🖥️

--- MainMenu.py
+++ MainMenu.py
@@ -1,7 +1,7 @@
 #!/usr/bin
-#Copyright 2021 AllHackingTools
-#Written by : Misha Korzhik
-#Github     : http://github.com/mishakorzik
+#Copyright 2022 Omindu-Hacker-ToolX
+#Written by : Omindu Anjana
+#Github     : http://github.com/OMINDUANJANA
 
 import requests
 import os
@@ -10,7 +10,7 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools")
+os.system("cd && cd Omindu-Hacker-ToolX")
 os.system("bash Logo.sh")
 os.system("bash src/MenuOps.sh")
 os.system("python3 Files/get.py")
@@ -85,40 +85,40 @@ elif(op==13):
 elif(op==14):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/WordlistGeneratorMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python2 Files/WordlistGeneratorMenu.py")
 elif(op==15):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/XSSAttackMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python2 Files/XSSAttackMenu.py")
 elif(op==16):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/discordMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python2 Files/discordMenu.py")
 elif(op==17):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 Files/telegramMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python2 Files/telegramMenu.py")
 elif(op==18):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python3 .check/OtherToolConfig.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python3 .check/OtherToolConfig.py")
 elif(op==19):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python3 .check/TermuxPanelConfig.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python3 .check/TermuxPanelConfig.py")
 elif(op==20):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("cd && cd AllHackingTools && python2 .settings/settingsMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python2 .settings/settingsMenu.py")
 elif(op==21):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
- os.system("clear && cd && cd AllHackingTools && bash .settings/LICENSE.sh && cd && cd AllHackingTools && python3 src/Timer2.py && python2 MainMenu.py")
+ os.system("clear && cd && cd Omindu-Hacker-ToolX && bash .settings/LICENSE.sh && cd && cd Omindu-Hacker-ToolX && python3 src/Timer2.py && python2 MainMenu.py")
 elif(op==22):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
  time.sleep(1)
- os.system("cd && cd AllHackingTools && python3 .check/UpdaterConfig.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && python3 .check/UpdaterConfig.py")
 elif(op==23):
  os.system("bash src/Inf.sh")
  time.sleep(0.3)
@@ -131,12 +131,12 @@ elif(op==13324715):
  os.system("cd && cd AllHackingTools && mv MainMenu.py /data/data/com.termux/files/home/AllHackingTools/.temp/temp && cd .settings && cd debug && cp MainMenu.py /data/data/com.termux/files/home/AllHackingTools/")
  print("[DEBUG] Warning! Customization has been disabled.")
 elif(op==24):
- os.system("clear && cd && cd AllHackingTools && bash Logo.sh")
+ os.system("clear && cd && cd Omindu-Hacker-ToolX && bash Logo.sh")
  print("\033[1;31;40mExiting System...")
  time.sleep(0.7)
 else:
  print("\033[1;31;40mInvalid input. Reloading Tools") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTool")
+ os.system("cd Omindu-Hacker-ToolX")
  os.system("python2 MainMenu.py")
