57840fca4f6bff049f88142d3ec16999ca4eaf04
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 11:49:26 +0530

    Update RunLogo.sh

--- src/RunLogo.sh
+++ src/RunLogo.sh
@@ -6,7 +6,7 @@ echo '
  |  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██║██║╚████║██║░░╚██╗╚════╝░░░██║░░░██║░░██║██║░░██║██║░░░░░
  |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
  |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.1
-[+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Title    : Omindu-Hacker-ToolX - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
 [+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
-[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+] Coded By : Omindu Anjana (Black Alpha)  -  All materials are provided for educational purposes only! 
 [+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]'|lolcat -p 1.9
