3f9b590c210f3d2024db78a194a50ebe73869a67
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 12:14:08 +0530

    Update AnalystickFiles.sh

--- Files/AnalystickFiles.sh
+++ Files/AnalystickFiles.sh
@@ -1,17 +1,17 @@
 cd
 cd 
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 pip2 install termcolor
 git clone https://github.com/floriankunushevci/rang3r
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/TechnicalMujeeb/TM-scanner.git 
 cd TM-scanner 
 chmod +x * sh install.sh
 cd
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/Gameye98/AstraNmap
 cd
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
