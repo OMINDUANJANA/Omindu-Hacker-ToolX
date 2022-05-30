65280113d3dca750419b7a450c58211ebbf6741d
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 11:06:48 +0530

    🍒🍒

--- src/AllHackingTool.sh
+++ src/AllHackingTool.sh
@@ -1,6 +1,6 @@
 cd
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 python3 src/CheckVersion.py
 python3 .check/CheckServerYesAndNo.py
 sleep 3
@@ -11,11 +11,11 @@ b="\033[1;34m"
 w="\033[0m"
 cd
 cd
-cd AllHackingTools 
+cd Omindu-Hacker-ToolX 
 cd Tool
-mv MainMenu.py /data/data/com.termux/files/home/AllHackingTools
+mv MainMenu.py /data/data/com.termux/files/home/Omindu-Hacker-ToolX
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 clear
 echo -e $b"[ ✔ ]"$g"succesfull verifined"$w
 python2 MainMenu.py
