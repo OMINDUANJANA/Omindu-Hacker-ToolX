11bf03a83579ac42ca0064b734ec535fe7ba68f8
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 12:17:39 +0530

    Update AndroidMenu.py

--- Files/AndroidMenu.py
+++ Files/AndroidMenu.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 os.system("clear")
-os.system("cd && cd AllHackingTools && clear && bash Logo.sh")
+os.system("cd && cd Omindu-Hacker-ToolX && clear && bash Logo.sh")
 
 print("  \033[1;34m[ 01 ] >> \033[1;36;40mPyshell - Remote Access Trojan - RAT")
 print("  \033[1;34m[ 02 ] >> \033[1;36;40mWishfish - Powerful Tool For Grab Front Camera Snap Using A Link")
@@ -18,27 +18,27 @@ op=int(raw_input("R6moteTRo1anR8t: "))
 
 if(op==1):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd pyshell && ./Pyshell && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && cd pyshell && ./Pyshell && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==2):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd wishfish && ./wishfish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && cd wishfish && ./wishfish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==3):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd lockphish && bash lockphish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && cd lockphish && bash lockphish.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==4):
  os.system("clear")
- os.system("cd && cd AllHackingTools && cd Files && bash HatCloud.sh && cd && cd AllHackingTools && python2 MainMenu.py")
+ os.system("cd && cd Omindu-Hacker-ToolX && cd Files && bash HatCloud.sh && cd && cd AllHackingTools && python2 MainMenu.py")
 elif(op==5):
  time.sleep(0.2)
  print("\033[1;31;40mQuiting System...")
  sys.exit()
 elif(op==6):
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd Omindu-Hacker-ToolX")
  os.system("python2 MainMenu.py")
 else:
  print("\033[1;31;40mInvalid input. Reloading Tools") 
  time.sleep(1.6)
  os.system("cd")
- os.system("cd AllHackingTools")
+ os.system("cd Omindu-Hacker-ToolX")
  os.system("python2 Files/AndroidMenu.py")
