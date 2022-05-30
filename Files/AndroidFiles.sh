1ef6773ac13de6fccac55d8600f3b067e1914c03
Omindu Anjana <93399269+OMINDUANJANA@users.noreply.github.com>
2022-05-30 12:15:11 +0530

    Update AndroidFiles.sh

--- Files/AndroidFiles.sh
+++ Files/AndroidFiles.sh
@@ -1,19 +1,19 @@
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/knassar702/pyshell
 cd Pyshell
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/kinghacker0/WishFish
 cd wishfish
 cd 
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/JasonJerry/lockphish
 cd lockphish
 cd
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
 git clone https://github.com/HatBashBR/HatCloud
 cd
 cd
-cd AllHackingTools
+cd Omindu-Hacker-ToolX
